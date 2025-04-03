b297456fe69fed8638848ca2504776a1814f6936
anujwarsaw <109848716+max420max@users.noreply.github.com>
2025-04-03 15:01:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -20,3 +20,21 @@ def get_llm_hf_inference(model_id=model_id, max_new_tokens=128, temperature=0.1)
         token = os.getenv("HF_TOKEN")
     )
     return llm
+    
+# Initialize session state for avatars
+if "avatars" not in st.session_state:
+    st.session_state.avatars = {'user': None, 'assistant': None}
+​
+# Initialize session state for user text input
+if 'user_text' not in st.session_state:
+    st.session_state.user_text = None
+​
+# Initialize session state for model parameters
+if "max_response_length" not in st.session_state:
+    st.session_state.max_response_length = 128
+​
+if "system_message" not in st.session_state:
+    st.session_state.system_message = "friendly AI conversing with a human user"
+​
+if "starter_message" not in st.session_state:
+    st.session_state.starter_message = "Hello, there! How can I help you today?"
